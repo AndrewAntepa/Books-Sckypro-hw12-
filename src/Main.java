a8883d3847a7c554e79362c01e4cb7d52aec3c2d
AndrewAntepa <andrewantepa@gmail.com>
2024-11-18 21:19:35 +0800

    hw12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author pushkin = new Author("Aleksandr", "Pushkin" );
+        Author tolstoy = new Author("Leo", "Tolstoy" );
+        Book dubrovskiy = new Book("Дубровский", pushkin, 1841);
+        Book warAndPeace = new Book("Война и мир", tolstoy, 1863);
+
+        System.out.println(dubrovskiy);
+        System.out.println(warAndPeace);
+
+        dubrovskiy.setYear(1234);
+        System.out.println(dubrovskiy);
     }
 }
