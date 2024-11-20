fd464f15e155fa319059073ab7177786082318c3
AndrewAntepa <andrewantepa@gmail.com>
2024-11-20 21:17:11 +0800

    hw13

--- src/Author.java
+++ src/Author.java
@@ -29,7 +29,6 @@ public class Author {
         Author author = (Author) o;
         return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(name, surname);
--- src/Book.java
+++ src/Book.java
@@ -35,7 +35,6 @@ public class Book {
                 ", year=" + year +
                 '}';
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
